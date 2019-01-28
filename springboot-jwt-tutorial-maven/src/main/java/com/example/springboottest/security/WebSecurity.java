aedbb4bcb4a21e8b995001281a81c2da39a6f4d4
os_kraisaek.tha <os_kraisaek.tha@os_kraisaek.tha>
2019-01-28 17:14:23 +0700

    enable websecurity

--- springboot-jwt-tutorial-maven/src/main/java/com/example/springboottest/security/WebSecurity.java
+++ springboot-jwt-tutorial-maven/src/main/java/com/example/springboottest/security/WebSecurity.java
@@ -1,8 +1,10 @@
 package com.example.springboottest.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +15,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import com.example.springboottest.constant.SecurityConstants;
 import com.example.springboottest.service.UserDetailsServiceImpl;
 
+@EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
 	private UserDetailsServiceImpl userDetailsService;
@@ -40,6 +43,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		
 	}
 	
+	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
